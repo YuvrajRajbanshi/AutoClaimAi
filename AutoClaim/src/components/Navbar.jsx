16fdb404584ea13af5df76b49b0ca932d51af5a1
YuvrajRajbanshi <singhraj0461@gmail.com>
2025-03-04 02:13:57 +0530

    minor changes in UI

--- AutoClaim/src/components/Navbar.jsx
+++ AutoClaim/src/components/Navbar.jsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
-import { FaCarCrash, FaCameraRetro, FaSignInAlt, FaUserPlus } from "react-icons/fa";
+import {
+  FaCarCrash,
+  FaCameraRetro,
+  FaSignInAlt,
+  FaUserPlus,
+} from "react-icons/fa";
 
 const Navbar = () => {
   return (
@@ -13,16 +18,28 @@ const Navbar = () => {
 
         {/* Navigation Menu */}
         <div className="flex space-x-6">
-          <Link to="/" className="hover:text-blue-400">Home</Link>
-          <Link to="/upload" className="hover:text-blue-400">Analyze Damage</Link>
-          <Link to="/thermal-scanner" className="text-yellow-400 font-semibold hover:text-yellow-300">
-            <FaCameraRetro className="mr-1" /> Thermal Scanner
+          <Link to="/" className="hover:text-blue-400">
+            Home
           </Link>
-          <Link to="/contact" className="hover:text-blue-400">Contact</Link>
+          <Link to="/upload" className="hover:text-blue-400">
+            Analyze Damage
+          </Link>
+          {/* <Link
+            to="/thermal-scanner"
+            className="text-yellow-400 font-semibold hover:text-yellow-300"
+          >
+            <FaCameraRetro className="mr-1" /> Thermal Scanner
+          </Link> */}
+          <Link to="/contact" className="hover:text-blue-400">
+            Contact
+          </Link>
           <Link to="/login" className="flex items-center hover:text-green-400">
             <FaSignInAlt className="mr-1" /> Login
           </Link>
-          <Link to="/register" className="flex items-center hover:text-yellow-400">
+          <Link
+            to="/register"
+            className="flex items-center hover:text-yellow-400"
+          >
             <FaUserPlus className="mr-1" /> Register
           </Link>
         </div>
--- AutoClaim/src/pages/Home.jsx
+++ AutoClaim/src/pages/Home.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import InteractiveGridPattern from "../components/magicui/InteractiveGridPattern";
 import { Link } from "react-router-dom";
+import {
+  FaCarCrash,
+  FaCameraRetro,
+  FaSignInAlt,
+  FaUserPlus,
+} from "react-icons/fa";
 
 const Home = () => {
   return (
@@ -14,13 +20,26 @@ const Home = () => {
 
       <img
         src="https://source.unsplash.com/800x400/?car,accident"
-        alt="Car Damage"
+        // alt="Car Damage"
+        style={{ display: "none" }}
         className="mt-6 rounded-lg shadow-lg z-10"
       />
 
-      <Link to="/upload" className="mt-6 px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 z-10">
-        Analyze Damage Now
-      </Link>
+      <div className="flex flex-row gap-10 items-center mt-6 space-y-4 z-10">
+        <Link
+          to="/upload"
+          className="mt-6 px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 z-10"
+        >
+          Analyze Damage Now
+        </Link>
+        <Link
+          to="/thermal-scanner"
+          className="mt-4 px-4 py-[15px] bg-amber-900    text-white rounded-lg hover:bg-gray-600 z-10  flex items-center justify-between gap-2"
+        >
+          <FaCameraRetro className="text-yellow-600  text-4 text-center " />{" "}
+          Thermal Scanner
+        </Link>
+      </div>
     </div>
   );
 };
